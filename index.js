649ac3e85b72bcc0a7ccbeb550d116308ef3fc24
Chandrakant Dahare <ckant912@gmail.com>
2022-09-16 16:55:25 +0530

    Admin & Employee Schema Added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// This is Index.js File
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const routes = require("./routes/routes");
+
+app = express();
+dotenv.config();
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.send("App is Running");
+});
+
+app.listen(port, () => {
+    console.log(`App is running on port:  ${port}`);
+});
+
+mongoose
+    .connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((error) => console.log(error));
